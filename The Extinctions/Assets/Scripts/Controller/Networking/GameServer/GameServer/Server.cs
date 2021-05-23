bc64b1c4eaba44218bd228abc6cd4a08123b1ac3
lausenen <lau.thomsen.privat@gmail.com>
2021-05-23 12:32:11 +0200

    Made half of first gameserver vid

---
+++ The Extinctions/Assets/Scripts/Controller/Networking/GameServer/GameServer/Server.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    public class Server
+    {
+        public static int MaxPlayers { get; private set; }
+        public static int Port { get; private set; }
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+        private static TcpListener _tcpListener;
+
+        public void Start(int _maxPlayers, int _port)
+        {
+            
+            Console.WriteLine("Starting server...");
+            MaxPlayers = _maxPlayers;
+            Port = _port;
+
+            _tcpListener = new TcpListener(IPAddress.Any, Port);
+            _tcpListener.Start();
+            _tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+            
+            Console.WriteLine($"Server started on {Port}.");
+        }
+
+        private static void TCPConnectCallback(IAsyncResult _result)
+        {
+            TcpClient _client = _tcpListener.EndAcceptTcpClient(_result);
+            _tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                if (clients[i].tcp.socket == null)
+                {
+                    clients[i].tcp.Connect(_client);
+                    return;
+                }
+            }
+            
+            Console.WriteLine($"{_client.Client.RemoteEndPoint} failed to connect: Server full!");
+        }
+
+        private static void InitializeServerData()
+        {
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                clients.Add(i, new Client(i));
+            }
+        }
+    }
+}
