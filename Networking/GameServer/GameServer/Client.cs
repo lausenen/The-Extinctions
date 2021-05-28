5113153c63628f7ff9d0445f4e2c812147706487
lausenen <lau.thomsen.privat@gmail.com>
2021-05-28 11:52:45 +0200

    first 5:30 min of second tutorial vid

--- Networking/GameServer/GameServer/Client.cs
+++ Networking/GameServer/GameServer/Client.cs
@@ -37,10 +37,27 @@ namespace GameServer
 
                 receiveBuffer = new byte[dataBufferSize];
                 stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+                ServerSend.Welcome(id, "Welcome to the server!");
                 
                 
             }
 
+            public void SendData(Packet _packet)
+            {
+                try
+                {
+                    if (socket != null)
+                    {
+                        stream.BeginWrite(_packet.ToArray(), 0, _packet.Length(), null, null);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error sending data to player {id} via TCP: {e}");
+                    throw;
+                }
+            }
+            
             private void ReceiveCallback(IAsyncResult _result)
             {
                 try
