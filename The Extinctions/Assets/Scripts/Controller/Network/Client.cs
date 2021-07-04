515daa7f17c2741c9a67d7e68049ab7df228c1ee
lausenen <lau.thomsen.privat@gmail.com>
2021-07-04 21:34:29 +0200

    Made Spawn all players

--- The Extinctions/Assets/Scripts/Controller/Network/Client.cs
+++ The Extinctions/Assets/Scripts/Controller/Network/Client.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class Client : MonoBehaviour
