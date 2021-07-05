30f31613a564fa4baff1cb8a6af4a263590c8b2e
lausenen <lau.thomsen.privat@gmail.com>
2021-07-05 12:06:47 +0200

    Player movement

---
+++ Networking/GameServer/GameServer/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace GameServer
+{
+    public class Player
+    {
+        public int id;
+        public string username;
+
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public Player(int id, string username, Vector3 position)
+        {
+            this.id = id;
+            this.username = username;
+            this.position = position;
+        }
+    }
+}
